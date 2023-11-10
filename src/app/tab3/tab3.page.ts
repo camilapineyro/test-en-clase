4ece2853edc2a1453d9c9a4f7fbaf181cd7c0b27
camilapineyro <131424829+camilapineyro@users.noreply.github.com>
2023-11-10 09:00:32 -0300

    Add files via upload

---
+++ src/app/tab3/tab3.page.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { CarritoService } from '../core/service/carrito.service';
+
+@Component({
+  selector: 'app-tab3',
+  templateUrl: 'tab3.page.html',
+  styleUrls: ['tab3.page.scss']
+})
+export class Tab3Page {
+
+  constructor(
+    public carritoService: CarritoService
+  ) {}
+
+}
